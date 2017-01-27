d98b7f7b71b1cfe1fb422efdfdde44976723a618
PiratedSoftwareCo <jackjack762@gmail.com>
2017-01-27 12:50:46 +0000

    Committed latest changes

--- Main/com/qa/jackchris/Address.java
+++ Main/com/qa/jackchris/Address.java
@@ -6,16 +6,55 @@ public class Address {
 	private String town;
 	private String county;
 	private String postalCode;
-	
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getTown() {
+		return town;
+	}
+
+	public void setTown(String town) {
+		this.town = town;
+	}
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
 	public Address(){}
 	public Address(String add1, String add2, String town, String county, String postCode){
-		addressLine1 = add1;
-		addressLine2 = add2;
-		this.town = town;
-		this.county = county;
-		this.postalCode = postCode;
+		setAddressLine1(add1);
+		setAddressLine2(add2);
+		setTown(town);
+		setCounty(county);
+		setPostalCode(postCode);
 	}
-	
 	
 	
 }
--- Main/com/qa/jackchris/Customer.java
+++ Main/com/qa/jackchris/Customer.java
@@ -1,6 +1,27 @@
 package com.qa.jackchris;
 
 public class Customer {
+	private String name;
+	private Address customerAddress;
+	
 	public Customer(){}
+	public Customer(String name){
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Address getCustomerAddress() {
+		return customerAddress;
+	}
+	public void setCustomerAddress(Address customerAddress) {
+		this.customerAddress = customerAddress;
+	}
 	
 }
